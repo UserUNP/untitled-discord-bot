a69b7ea54be57563a8608000c865ea1aecbdc708
UserUNP <39243708+UserUNP@users.noreply.github.com>
2022-12-07 05:47:34 +0100

    🐛 fixed subpath issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  integrations: [tailwind()]
+	publicDir: "./src/assets",
+	base: "/untitled-discord-bot",
+	site: "https://userunp.github.io",
+	integrations: [tailwind()]
 });
